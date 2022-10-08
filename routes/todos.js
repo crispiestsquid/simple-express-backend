734adfcb18f8c8127d6272fe314d0edacf05b7ea
Kevin Little <crispiestsquid@aol.com>
2022-10-08 18:47:48 -0400

    adding post route for todos

--- routes/todos.js
+++ routes/todos.js
@@ -2,21 +2,47 @@ const express = require('express');
 const router = express.Router();
 const Todo = require('../models/todo-schema');
 
-/* GET todos listing. */
-router.get('/', function (req, res, next) {
-	Todo.find()
-		.then(todos => {
-			if (todos) {
+/* Route for todos/ */
+router.route('/')
+	// GET
+	.get(function (req, res, next) {
+		Todo.find()
+			.then(todos => {
+				if (todos) {
+					res.setHeader('Content-Type', 'application/json');
+					res.statusCode = 200;
+					res.json({ todos });
+				} else {
+					res.setHeader('Content-Type', 'application/json');
+					res.statusCode = 500;
+					res.json({ error: 'Unknown error fetching todos... try again.' });
+				}
+			})
+			.catch(err => {
 				res.setHeader('Content-Type', 'application/json');
-				res.statusCode = 200;
-				res.json({ todos });
-			}
-		})
-		.catch(err => {
-			res.setHeader('Content-Type', 'application/json');
-			res.statusCode = 500;
-			res.json({ error: err });
-		});
-});
+				res.statusCode = 500;
+				res.json({ error: err });
+			});
+	})
+	// POST
+	.post(function (req, res, next) {
+		Todo.create(req.body)
+			.then(todo => {
+				if (todo) {
+					res.setHeader('Content-Type', 'application/json');
+					res.statusCode = 200;
+					res.json({ todo });
+				} else {
+					res.setHeader('Content-Type', 'application/json');
+					res.statusCode = 500;
+					res.json({ error: 'Unknown error creating todo... try again.' });
+				}
+			})
+			.catch(err => {
+				res.setHeader('Content-Type', 'application/json');
+				res.statusCode = 500;
+				res.json({ error: err });
+			});
+	});
 
 module.exports = router;
