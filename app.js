064c683d394655485ab6d478af0d3ef91f5e4138
Kevin Little <crispiestsquid@aol.com>
2022-10-08 20:27:01 -0400

    adding cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
@@ -18,6 +19,8 @@ connect.then(() => console.log('Connected correctly to the database!'),
   err => console.error(err, 'Error connecting to mongo...'));
 
 const app = express();
+
+app.use(cors());
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
